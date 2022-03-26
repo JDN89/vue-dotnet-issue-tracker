130157264d25809e3b4e9155a4bc7adc0912f669
JDN89 <jan.de.niels@gmail.com>
2022-03-26 16:32:40 -0500

    AddNewProject controller

--- WebUI/Controllers/ProjectController.cs
+++ WebUI/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
 using Application.DTOs;
+using Application.Handlers.Projects.Commands;
 using Application.Handlers.Projects.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +12,16 @@ public class ProjectController : BaseController
 {
     [HttpGet]
     [Authorize]
-    public async Task<ActionResult<List<GetProjectDto>>> Test()
+    public async Task<ActionResult<List<GetProjectDto>>> GetProjects()
     {
         return await Mediator.Send(new GetAllProjects.Query());
+    }
+
+    [HttpPost("{projectTitle}")]
+    [Authorize]
+    public async Task<IResult> AddProject(string projectTitle)
+    {
+        await Mediator.Send(new AddNewProject.Command {Title = projectTitle});
+        return Results.Ok();
     }
 }
