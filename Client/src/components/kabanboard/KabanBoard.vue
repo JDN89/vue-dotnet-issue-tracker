adb01254f26c89106ec83a4cfc3bcc9315b376b0
JDN89 <jan.de.niels@gmail.com>
2022-03-26 10:21:27 -0500

    eslint KabanBaord

--- Client/src/components/kabanboard/KabanBoard.vue
+++ Client/src/components/kabanboard/KabanBoard.vue
@@ -5,19 +5,27 @@
 <template>
   <div id="Kaban-Board" class="flex gap-3 flex-row justify-around flex-1">
     <div class="min-w-34 max-w-80">
-      <h1 text-3xl mb-5>Open</h1>
+      <h1 text-3xl mb-5>
+        Open
+      </h1>
       <OpenIssues />
     </div>
     <div class="min-w-24 max-w-80">
-      <h1 text-3xl mb-5>In Progress</h1>
+      <h1 text-3xl mb-5>
+        In Progress
+      </h1>
       <IssuesInProgress />
     </div>
     <div class="min-w-34 max-w-80">
-      <h1 text-3xl mb-5>In Review</h1>
+      <h1 text-3xl mb-5>
+        In Review
+      </h1>
       <ReviewIssues />
     </div>
     <div class="min-w-34 max-w-80">
-      <h1 text-3xl mb-5>Closed</h1>
+      <h1 text-3xl mb-5>
+        Closed
+      </h1>
       <ClosedIssues />
     </div>
   </div>
