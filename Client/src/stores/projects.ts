0b022fc94aa893a41402de422745a9fd82bb93f6
JDN89 <jan.de.niels@gmail.com>
2022-03-28 18:35:48 -0500

    add NewCreatedProject to the Projects stack in the store

--- Client/src/stores/projects.ts
+++ Client/src/stores/projects.ts
@@ -93,7 +93,7 @@ export const useProjectStore = defineStore({
         await eventService.addNewProject(userStore.getToken, newProject)
           .then((response) => {
             if (response.status === 200)
-              return true
+              return this.Projects?.push(response.data)
           }).catch((error) => {
             if (axios.isAxiosError(error)) {
               if (error.response) {
@@ -102,13 +102,9 @@ export const useProjectStore = defineStore({
                 console.log(error.response.headers)
               }
               else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
                 console.log(error.request)
               }
               else {
-                // Something happened in setting up the request that triggered an Error
                 console.log('Error', error.message)
               }
             }
