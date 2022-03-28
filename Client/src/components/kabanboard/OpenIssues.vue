acc2c63bf5650251d36a9eaf43d0effa5b2a2ad6
JDN89 <jan.de.niels@gmail.com>
2022-03-28 11:18:56 -0500

    remove commented out unused code

--- Client/src/components/kabanboard/OpenIssues.vue
+++ Client/src/components/kabanboard/OpenIssues.vue
@@ -4,25 +4,7 @@ import draggable from 'vuedraggable'
 import { useProjectStore } from '~/stores/projects'
 
 const store = useProjectStore()
-/* watch(() => store.OpenIssues, (newValue) => {
-  console.log(newValue)
 
-},
-
-) */
-/*
-watcher store in onBeforeUpdate because:
-the watch functions were loaded before the pinia store were installed
-so the functions returned (watch: null) AND SEIZED working!!
-so now on updates we watch the stores for changes
-EDIT: triggers function and updates the correct DB?
-but edit doesn't need to trigger and update event of the whole table, neither does DB
-HOW TO SOLVE THIS??
-
-on update, if @change vue draggable (move.index) == ture => watch the projectStore for changes
-and update if necesarry!
-if @change (move.index) == true
-*/
 onBeforeUpdate(async() => {
   watch(store.OpenIssues!, async(value) => {
     await store.updateAllOpenIssues(value)
