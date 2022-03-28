4a44debe67207ef98156c96adb95ef05bf403cc4
JDN89 <jan.de.niels@gmail.com>
2022-03-28 12:43:59 -0500

    built out edit button logic

--- Client/src/components/button/EditButton.vue
+++ Client/src/components/button/EditButton.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 
 const isHidden = ref(true)
+const showHidden = () => {
+  isHidden.value = false
+}
+const edit = (item: string) => {
+  console.log(item)
+}
 const editArr: string [] = ['Edit', 'Delete']
 </script>
 
 <template>
-  <button v-if="isHidden" i-bx-dots-vertical-rounded />
+  <button v-if="isHidden" i-bx-dots-vertical-rounded @click="showHidden" />
   <ul v-else>
-    <li v-for="item in editArr" :key="item">
+    <li v-for="item in editArr" :key="item" @click="edit(item)">
       {{ item }}
     </li>
   </ul>
