f7b1cf3de81e727a99a2e5298172168b4cac8447
JDN89 <jan.de.niels@gmail.com>
2022-03-29 14:05:08 -0500

    finish styling edit button

--- Client/src/components/button/EditButton.vue
+++ Client/src/components/button/EditButton.vue
@@ -7,9 +7,9 @@ const isHidden = ref(true)
 
 <template>
   <button v-if="isHidden" class="float-right" i-bx-dots-vertical-rounded @click="isHidden=false" />
-  <div v-else class="flex  ">
+  <div v-else class="flex flex-col justify-end">
     <ul @click="isHidden=true">
-      <li v-for="(option,index) in editOptions" :key="index" class="btn" @click="$emit('edit',option)">
+      <li v-for="(option,index) in editOptions" :key="index" class=" min-w-10 max-w-21 btn m-1 float-right" @click="$emit('edit',option)">
         {{ option }}
       </li>
     </ul>
