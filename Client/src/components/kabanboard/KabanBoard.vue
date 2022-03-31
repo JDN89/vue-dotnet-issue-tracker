3a9b7ec1ade576ae45f40182f44e1e873c97de72
JDN89 <jan.de.niels@gmail.com>
2022-03-31 14:14:43 -0500

    add FocusedIssue to bottom kabanBoard

--- Client/src/components/kabanboard/KabanBoard.vue
+++ Client/src/components/kabanboard/KabanBoard.vue
@@ -1,33 +1,38 @@
 <script setup lang="ts">
 
+import { useProjectStore } from '~/stores/projects'
+const store = useProjectStore()
 </script>
 
 <template>
-  <div id="Kaban-Board" class="flex gap-3 flex-row justify-around flex-1">
-    <div class="min-w-34 max-w-80">
-      <h1 text-3xl mb-5>
-        Open
-      </h1>
-      <OpenIssues />
+  <div>
+    <div id="Kaban-Board" class="flex gap-3 flex-row justify-around flex-1">
+      <div class="min-w-34 max-w-80">
+        <h1 text-3xl mb-5>
+          Open
+        </h1>
+        <OpenIssues />
+      </div>
+      <div class="min-w-24 max-w-80">
+        <h1 text-3xl mb-5>
+          In Progress
+        </h1>
+        <IssuesInProgress />
+      </div>
+      <div class="min-w-34 max-w-80">
+        <h1 text-3xl mb-5>
+          In Review
+        </h1>
+        <ReviewIssues />
+      </div>
+      <div class="min-w-34 max-w-80">
+        <h1 text-3xl mb-5>
+          Closed
+        </h1>
+        <ClosedIssues />
+      </div>
     </div>
-    <div class="min-w-24 max-w-80">
-      <h1 text-3xl mb-5>
-        In Progress
-      </h1>
-      <IssuesInProgress />
-    </div>
-    <div class="min-w-34 max-w-80">
-      <h1 text-3xl mb-5>
-        In Review
-      </h1>
-      <ReviewIssues />
-    </div>
-    <div class="min-w-34 max-w-80">
-      <h1 text-3xl mb-5>
-        Closed
-      </h1>
-      <ClosedIssues />
-    </div>
+    <FocusedIssue v-if="store.getShowfocusedIssue" />
   </div>
 </template>
 
