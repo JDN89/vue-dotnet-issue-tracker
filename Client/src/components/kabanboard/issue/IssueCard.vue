ed1c0acd08e2a8336547a4f91ee7b7ff9bfcd844
JDN89 <jan.de.niels@gmail.com>
2022-03-31 14:21:43 -0500

    remove unused div from IssueCard

--- Client/src/components/kabanboard/issue/IssueCard.vue
+++ Client/src/components/kabanboard/issue/IssueCard.vue
@@ -15,8 +15,11 @@ const focusedIssue: FocusedIssue|null = reactive({
   urgency: props.urgency,
 })
 
-const storeFocusedIssueInfo = (issue: FocusedIssue) => {
+const storeFocusedIssueInfo = async(issue: FocusedIssue) => {
   store.FocusedIssue = focusedIssue
+  console.log(focusedIssue)
+
+  store.ShowFocusedIssue = true
 }
 const myUrgencyStyles = new Map<string, string>([['Medium', 'border-1'], ['Low', 'border-dotted dark:border-dotted'], ['High', 'border-2 dark:border-2 ']])
 
@@ -26,25 +29,23 @@ const urgencyStyle = computed(() => {
 </script>
 
 <template>
-  <div>
-    <div @click="storeFocusedIssueInfo(focusedIssue)">
-      <div class="square-border" :class="`${urgencyStyle}`">
-        <div
-          class=" flex justify-between m-3"
-        >
-          <p class="font-semibold font-sans tracking-wide text-sm">
-            {{ props.title }}
-          </p>
-          <Urgency :urgency="props.urgency">
-            {{ props.urgency.toUpperCase() }}
-          </Urgency>
-        </div>
-        <div class="flex m-2 justify-between items-center">
-          <span class="text-sm text-gray-600 dark:text-gray-300">{{ props.date }}</span>
-          <Badge :type="props.type">
-            {{ props.type }}
-          </Badge>
-        </div>
+  <div @click="storeFocusedIssueInfo(focusedIssue)">
+    <div class="square-border" :class="`${urgencyStyle}`">
+      <div
+        class=" flex justify-between m-3"
+      >
+        <p class="font-semibold font-sans tracking-wide text-sm">
+          {{ props.title }}
+        </p>
+        <Urgency :urgency="props.urgency">
+          {{ props.urgency.toUpperCase() }}
+        </Urgency>
+      </div>
+      <div class="flex m-2 justify-between items-center">
+        <span class="text-sm text-gray-600 dark:text-gray-300">{{ props.date }}</span>
+        <Badge :type="props.type">
+          {{ props.type }}
+        </Badge>
       </div>
     </div>
   </div>
