82c552f098c287dd997bd55da5275f18912b6a76
JDN89 <jan.de.niels@gmail.com>
2022-03-31 13:35:31 -0500

    new migrations containing new issueId name prop

--- Infrastructure/Migrations/20220331181610_testChangeId.Designer.cs
+++ Infrastructure/Migrations/20220331181610_testChangeId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220331181610_testChangeId")]
+    partial class testChangeId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Migrations/20220331181610_testChangeId.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class testChangeId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OpenIssueId",
+                table: "OpenIssues",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "IssueInReviewId",
+                table: "IssuesInReview",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "InProgressIssueId",
+                table: "InProgressIssues",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ClosedIssueId",
+                table: "ClosedIssues",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "OpenIssues",
+                newName: "OpenIssueId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "IssuesInReview",
+                newName: "IssueInReviewId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "InProgressIssues",
+                newName: "InProgressIssueId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "ClosedIssues",
+                newName: "ClosedIssueId");
+        }
+    }
+}
