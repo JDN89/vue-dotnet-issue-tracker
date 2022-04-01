3f08b6b57476ef1c02fecc5fa881f2d0ff040034
JDN89 <jan.de.niels@gmail.com>
2022-04-01 12:16:33 -0500

    change min-w-lg focusedIssue

--- Client/src/components/kabanboard/issue/FocusedIssue.vue
+++ Client/src/components/kabanboard/issue/FocusedIssue.vue
@@ -39,7 +39,7 @@ const uploadEdditedIssue = () => {
     class="flex bg-gray-700 bg-opacity-50 fixed left-0 right-0 bottom-0 top-0 items-center"
   >
     <div
-      class=" min-h-44 min-w-auto max-h-auto w-full m-1 p-2 sm:max-w-70 content-center sm:mx-auto mx-auto"
+      class=" min-h-44 min-w-lg w-auto max-h-auto w-full m-1 p-2 sm:max-w-70 content-center sm:mx-auto mx-auto"
     >
       <div class="square-border bg-light-500" :class="`${urgencyStyle}`">
         <div
