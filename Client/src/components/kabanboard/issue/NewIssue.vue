a58378643e50d73ad2fda2d9d39cba55a799bdca
JDN89 <jan.de.niels@gmail.com>
2022-04-23 18:47:43 -0500

    format text

--- Client/src/components/kabanboard/issue/NewIssue.vue
+++ Client/src/components/kabanboard/issue/NewIssue.vue
@@ -38,7 +38,7 @@ const updateType = (type: string) => {
   newIssue.type = type
   issueTypesHidden.value = true
 }
-const addNewIssue = async() => {
+const addNewIssue = async () => {
   if (newIssue.title.length === 0 || newIssue.description.length === 0) {
     return alert('title and or description or missing!')
   }
@@ -52,28 +52,19 @@ const addNewIssue = async() => {
 
 <template>
   <div class="flex bg-gray-700 bg-opacity-50 fixed left-0 right-0 bottom-0 top-0 items-center">
-    <div
-      class=" min-w-lg h-auto m-1 p-2 sm:max-w-70 content-center sm:mx-auto mx-auto"
-    >
+    <div class=" min-w-lg h-auto m-1 p-2 sm:max-w-70 content-center sm:mx-auto mx-auto">
       <div class="square-border flex-col  h-auto w-auto bg-light-500" :class="`${urgencyStyle}`">
         <div class="flex h-auto justify-between m-3">
           <i i-carbon-fetch-upload-cloud class="cursor-pointer" @click="addNewIssue" />
           <div v-if="urgencyOptionsHidden" class="flex cursor-pointer justify-around">
-            <Urgency
-              urgency="newIssue.urgency"
-              @click="showUrgencyOptions"
-            >
+            <Urgency urgency="newIssue.urgency" @click="showUrgencyOptions">
               {{ newIssue.urgency.toUpperCase() }}
             </Urgency>
           </div>
           <div v-else>
             <ul class="flex flex-col ">
-              <li
-                v-for="(urgency ,index) in urgencyOptions"
-                :key="index"
-                class="min-w-10 max-w-21 btn m-1 "
-                @click="updateUrgency(urgency)"
-              >
+              <li v-for="(urgency, index) in urgencyOptions" :key="index" class="min-w-10 max-w-21 btn m-1 "
+                @click="updateUrgency(urgency)">
                 {{ urgency }}
               </li>
             </ul>
@@ -85,34 +76,22 @@ const addNewIssue = async() => {
           </div>
           <div v-else>
             <ul class="flex flex-col justify-end">
-              <li
-                v-for="(type ,index) in IssueTypes"
-                :key="index"
-                class="min-w-10 max-w-39 btn m-1 float-right"
-                @click="updateType(type)"
-              >
+              <li v-for="(type, index) in IssueTypes" :key="index" class="min-w-10 max-w-39 btn m-1 float-right"
+                @click="updateType(type)">
                 {{ type }}
               </li>
             </ul>
           </div>
-          <i i-carbon-close class="cursor-pointer" @click="store.ShowNewIssue=false" />
+          <i i-carbon-close class="cursor-pointer" @click="store.ShowNewIssue = false" />
         </div>
         <div class="flex justify-start w-auto">
-          <input
-            v-model="newIssue.title"
-            type="text"
-            placeholder="Add a Title"
-            class="bg-transparent focus:outline-none"
-          >
+          <input v-model="newIssue.title" type="text" placeholder="Add a Title"
+            class="bg-transparent focus:outline-none">
         </div>
 
         <div class=" flex w-auto ">
-          <textarea
-            v-model="newIssue.description"
-
-            placeholder="Add a description (drag corner to expand texterea)"
-            class="text-base focus:outline-none resize-y flex-grow bg-transparent"
-          />
+          <textarea v-model="newIssue.description" placeholder="Add a description (drag corner to expand texterea)"
+            class="text-base focus:outline-none resize-y flex-grow bg-transparent" />
         </div>
       </div>
     </div>
