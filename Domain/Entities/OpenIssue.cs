f27d899b5ebf61834a9f0943591f7504310db40e
JDN89 <jan.de.niels@gmail.com>
2022-04-23 13:16:26 -0500

    add [jsonIgnor] to fix: JsonException: A possible object cycle was detected

--- Domain/Entities/OpenIssue.cs
+++ Domain/Entities/OpenIssue.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Domain.Entities;
 
 public class OpenIssue : BaseIssue
 {
     public Guid Id { get; set; }
+    
+    [JsonIgnore] 
     public Project Project { get; set; }
     public Guid ProjectId { get; set; }
 }
