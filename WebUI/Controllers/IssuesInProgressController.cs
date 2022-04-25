8e5f187801ef4c79822b0a0e42dd7c4d1c0825b4
JDN89 <jan.de.niels@gmail.com>
2022-04-25 14:14:32 -0500

    add DelteIssueInProgress Delete method to InProgressIssue controller

--- WebUI/Controllers/IssuesInProgressController.cs
+++ WebUI/Controllers/IssuesInProgressController.cs
@@ -1,4 +1,3 @@
-using Application.DTOs;
 using Application.DTOs.Issues;
 using Application.Handlers.Issues.Commands;
 using Application.Handlers.Issues.Queries;
@@ -9,24 +8,36 @@ namespace WebUI.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class IssuesInProgressController: BaseController
+public class IssuesInProgressController : BaseController
 {
-     [HttpGet("{projectId}")]
-        [Authorize]
-        public async Task<ActionResult<List<GetIssueInProgressDto>>> GetAllIssues(Guid projectId)
+    [HttpGet("{projectId}")]
+    [Authorize]
+    public async Task<ActionResult<List<GetIssueInProgressDto>>> GetAllIssues(Guid projectId)
+    {
+        return await Mediator.Send(new GetAllIssuesInProgress.Query { ProjId = projectId });
+    }
+
+    [HttpPut("{projectId}")]
+    [Authorize]
+    public async Task<IResult> UpdateAllIssuesInProgress([FromBody] List<GetIssueInProgressDto> inProgressIssues, Guid projectId)
+    {
+
+        await Mediator.Send(new DeleteIssuesInProgress.Command { ProjectId = projectId });
+        if (inProgressIssues.Count <= 0) return Results.Ok();
+        await Mediator.Send(new AddIssuesInProgress.Command { Issues = inProgressIssues });
+
+        return Results.Ok();
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<IResult> DeleteOpenIssue(Guid id)
+    {
+        await Mediator.Send(new DeleteIssueInProgress.Command
         {
-            return await Mediator.Send(new GetAllIssuesInProgress.Query{ProjId = projectId});
-        }
-        
-         [HttpPut("{projectId}")]
-            [Authorize]
-            public async Task<IResult> UpdateAllIssuesInProgress([FromBody] List<GetIssueInProgressDto> inProgressIssues, Guid projectId)
-            {
-                
-               await Mediator.Send(new DeleteIssuesInProgress.Command{ProjectId = projectId});
-               if (inProgressIssues.Count<=0) return Results.Ok();
-               await Mediator.Send(new AddIssuesInProgress.Command {Issues = inProgressIssues});
-               
-               return Results.Ok();
-            }
+            Id = id
+        });
+        return Results.Ok();
+
+    }
 }
