66779b890446a2ff41800fd5728e2f4e2813cbd1
JDN89 <jan.de.niels@gmail.com>
2022-04-27 16:20:42 -0500

    remove token validation logic from app.vue and place in router guard

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
 import { useUserStore } from './stores/users'
 
 onBeforeMount(async() => {
-  const router = useRouter()
   const userStore = useUserStore()
   const token = window.localStorage.getItem('token')
   if (token)
     userStore.token = token
-  const username = window.localStorage.getItem('username')
-
-  if (token && username)
-    await router.push(`/hi/${encodeURIComponent(username)}`)
 })
 
 </script>
