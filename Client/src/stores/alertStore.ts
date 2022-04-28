30cb2eeb418bc6e644da393b1de740d37ee39b6e
JDN89 <jan.de.niels@gmail.com>
2022-04-28 15:25:05 -0500

    add types to alertstore.ts

--- Client/src/stores/alertStore.ts
+++ Client/src/stores/alertStore.ts
@@ -1,27 +1,27 @@
 
 import { acceptHMRUpdate, defineStore } from "pinia";
 
-interface AlertStateInterface {
-  showAlert: boolean;
-  alertMessage: string | null;
+interface State {
+    showAlert: boolean;
+    alertMessage: string | null;
 }
 
 export const useAlertStore = defineStore("alert", {
-  state: (): AlertStateInterface => ({
-    showAlert: false,
-    alertMessage: null,
-  }),
-  actions: {
-    async closeAlert() {
-      this.showAlert = false;
-      this.alertMessage = null;
+    state: (): State => ({
+        showAlert: false,
+        alertMessage: null,
+    }),
+    actions: {
+        async closeAlert() {
+            this.showAlert = false;
+            this.alertMessage = null;
+        },
     },
-  },
-  getters: {
-    getShowAlert: (state) => state.showAlert,
-    getAlertMessage: (state) => state.alertMessage,
-  },
+    getters: {
+        getShowAlert: (state: State) => state.showAlert,
+        getAlertMessage: (state: State) => state.alertMessage,
+    },
 });
 
 if (import.meta.hot)
-  import.meta.hot.accept(acceptHMRUpdate(useAlertStore, import.meta.hot));
+    import.meta.hot.accept(acceptHMRUpdate(useAlertStore, import.meta.hot));
