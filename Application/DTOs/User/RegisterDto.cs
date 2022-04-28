0bd4a352a6f56c6883528f77ae2ed6c41eba7bf8
JDN89 <jan.de.niels@gmail.com>
2022-04-28 10:13:26 -0500

    remove nullable ? in DTO objects

--- Application/DTOs/User/RegisterDto.cs
+++ Application/DTOs/User/RegisterDto.cs
@@ -4,14 +4,14 @@ namespace Application.DTOs.User;
 
 public class RegisterDto
 {
-    [Required] 
-    public string? Username { get; set; }
-    [Required] 
-    [EmailAddress] 
-    public string? Email { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
 
     [Required]
     // At least one number, lowerCase,upperCase and length between 4 and 8 char
     [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
-    public string? Password { get; set; }
+    public string Password { get; set; }
 }
