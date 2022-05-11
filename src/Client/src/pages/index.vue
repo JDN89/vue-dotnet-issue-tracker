d243a4a6131194e0424b2a63eadad2e8eda227d0
JDN89 <jan.de.niels@gmail.com>
2022-05-11 10:33:55 -0500

    rewrite part of the about page

--- src/Client/src/pages/index.vue
+++ src/Client/src/pages/index.vue
@@ -3,43 +3,47 @@
 </script>
 
 <template>
-  <header-guest class="mb-8" />
-  <div class="flex mx-auto">
-    <div class="m-auto content-center justify-center max-w-xl">
+    <header-guest class="mb-8" />
+    <div class="flex mx-auto">
+        <div class="m-auto content-center justify-center max-w-xl">
 
-      <h1 class="text-4xl p-3 text-left ">
-        About
-      </h1>
-      <p class="p-3 text-left">
+            <h1 class="text-4xl p-3 text-left ">
+                About
+            </h1>
+            <p class="p-3 text-left">
 
-        This is a Project I made to practice Clean Architecture (Robert C. Martin )
-        <br> thin Controllers using MediatR with ASP.NET MVC
-        <br>
-        <br>
-        Inspired by the drag and drop functionality of Notion which I tried to incorporate in this project.
+                This is a Project I made to practice Clean Architecture (Robert C. Martin )
+                <br>and thin MVC Controllers using the MediatR package with ASP.NET Core 6.0
+                <br>
+                <br>
+                Inspired by the drag and drop functionality of the Notion web app which I tried to incorporate into this
+                project.
 
-      </p>
+            </p>
 
-      <h1 class="prose text-4xl m-4 text-left pt-2">Usage</h1>
-      <p class="text-left">
-        Create a fake account or use the test account (jan@test.com - Pa$$w0rd) login and create a project to track to
-        track bugs and feature requests during the
-        projects
-        development.
-      </p>
+            <h1 class="text-4xl p-3 text-left pt-8">Usage</h1>
+            <p class=" p-3 text-left">
+                Create an account(fake email must:w
+                include @ and .com) or use the test account (jan@test.com - Pa$$w0rd),
+                login and create a project to
+                track
+                bugs and feature requests during the
+                projects
+                development.
+            </p>
 
+        </div>
     </div>
-  </div>
 
 </template>
 
 
 <style scoped>
 li {
-  text-align: left;
+    text-align: left;
 }
 
 li::before {
-  background-color: dark;
+    background-color: dark;
 }
 </style>
